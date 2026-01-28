e61f51d1d358a4002e32e01c37279c092e41e519
chirannnn <chiranyt117@gmail.com>
2026-01-29 01:09:22 +0530

    update node auth

---
+++ 07-nodejs-auth/server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+const express = require("express");
+const connectToDB = require("./database/db");
+const authRoutes = require("./routes/auth-routes");
+
+// connect to DB
+connectToDB();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middlewares
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is now listening on PORT ${PORT}`);
+});
