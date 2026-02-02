73ff6d6db90dda98eb0f00f88cec0470ab451311
chirannnn <chiranyt117@gmail.com>
2026-02-03 01:03:33 +0530

    update nodejs-auth

--- 07-nodejs-auth/routes/auth-routes.js
+++ 07-nodejs-auth/routes/auth-routes.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const { registerUser, loginUser } = require("../controllers/auth-controller");
+const {
+  registerUser,
+  loginUser,
+  changePassword,
+} = require("../controllers/auth-controller");
+const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 //all routes are related to authentication & authorization
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/change-password", authMiddleware, changePassword);
 
 module.exports = router;
