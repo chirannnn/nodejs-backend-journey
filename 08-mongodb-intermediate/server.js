6716f3fd9114b3e4f03be65c2e6f5f3b43c6a066
chirannnn <chiranyt117@gmail.com>
2026-02-08 00:35:46 +0530

    update mongodb-inter

--- 08-mongodb-intermediate/server.js
+++ 08-mongodb-intermediate/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const connectToDB = require("./database/db");
 const productRoutes = require("./routes/product-routes");
+const bookRoutes = require("./routes/book-routes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,6 +13,7 @@ connectToDB();
 app.use(express.json());
 
 app.use("/products", productRoutes);
+app.use("/reference", bookRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is now running one PORT ${PORT}`);
